cd6a67b05600c41bb975a8f2569638a65f228f12
chenliangloveyou <ios_elite@163.com>
2017-11-24 09:29:14 +0800

    commit

---
+++ EasyShowView/EasyShowView.h
@@ -0,0 +1,13 @@
+//
+//  EasyShowView.h
+//  EasyShowViewDemo
+//
+//  Created by nf on 2017/11/24.
+//  Copyright © 2017年 chenliangloveyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EasyShowView : UIView
+
+@end
---
+++ EasyShowView/EasyShowView.m
@@ -0,0 +1,21 @@
+//
+//  EasyShowView.m
+//  EasyShowViewDemo
+//
+//  Created by nf on 2017/11/24.
+//  Copyright © 2017年 chenliangloveyou. All rights reserved.
+//
+
+#import "EasyShowView.h"
+
+@implementation EasyShowView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
